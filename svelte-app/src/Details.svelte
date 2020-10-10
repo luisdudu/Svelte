0a60f0c852d1ccb9dc923017cffd2ef3674a2060
Luís Eduardo <luis.eduardo.225@gmail.com>
2020-10-10 12:58:13 -0300

    Add order manager project.

---
+++ svelte-app/src/Details.svelte
@@ -0,0 +1,31 @@
+<div>
+	<h2 class="font-bold text-md text-tial-400">{ code }</h2>
+	<div class="flex flex-wrap bg-white border-b border-blue-tial-100">
+		<div class="flex w-full m-4">
+			<div class="flex items-center">
+				<div class="flex flex-col p-4">
+					<span class="font-light text-sm">Objeto saiu para entregar</span>
+					<div class="flex">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="flex w-full m-4">
+			<div class="flex items-center">
+				<div class="flex flex-col p-4">
+					<span class="font-light text-sm">Objeto entregue ao destinatário</span>
+					<div class="flex">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+	export let code
+</script>
+
+<style>
+
+</style>
